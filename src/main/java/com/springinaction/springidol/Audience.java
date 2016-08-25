c4644980464846a93c5d67eab77a9c1bcb995707
Poskotinova Lyubov Sergeevna <poskotinova-ls@fxclub.org>
2016-08-25 14:07:03 +0700

    Создание аспектов

--- src/main/java/com/springinaction/springidol/Audience.java
+++ src/main/java/com/springinaction/springidol/Audience.java
@@ -1,5 +1,7 @@
 package com.springinaction.springidol;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Audience {
     public void takeSeats() { // Перед выступлением
         System.out.println("The audience is taking their seats.");
@@ -10,10 +12,35 @@ public class Audience {
     }
 
     public void applaud() { // После выступления
-        System.out.println("CLAP CLAP CLAP CLAP CLAP");
+        System.out.println("applaud: CLAP CLAP CLAP CLAP CLAP");
     }
 
     public void demandRefund() { // После неудачного выступления
         System.out.println("Boo! We want our money back!");
+    }
+
+
+    // метод для использования в аспекте И до И После
+    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+        try {
+            //System.out.println("The audience is taking their seats.");
+            takeSeats();
+
+            //System.out.println("The audience is turning off their cellphones");
+            turnOffCellPhones();
+
+            long start = System.currentTimeMillis(); // Перед выступлением
+            joinpoint.proceed(); // Вызов целевого метода
+            long end = System.currentTimeMillis(); // После выступления
+
+            //System.out.println("CLAP CLAP CLAP CLAP CLAP");
+            applaud();
+
+            System.out.println("The performance took " + (end - start)  + " milliseconds.");
+
+        } catch (Throwable t) {
+            //System.out.println("Boo! We want our money back!");
+            demandRefund();
+        }
     }
 }
---
+++ src/main/java/com/springinaction/springidol/TestClass.java
@@ -0,0 +1,15 @@
+package com.springinaction.springidol;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestClass {
+    public static void main(String[] args) throws Exception{
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-idol-aop-03.xml");
+
+        Performer eddie = (Performer)ctx.getBean("eddie");
+        eddie.perform();
+
+    }
+}
+
